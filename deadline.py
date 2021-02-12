ebaa93f90cf389005d404d2ac4b86e93327dd042
Huw Williams <67016487+Wil10w@users.noreply.github.com>
2021-02-12 11:58:04 +0100

    Adding Beginner Library

---
+++ deadline.py
@@ -0,0 +1,20 @@
+current_hour = 12
+current_minute = 37
+current_section = "PM"
+due_hour = 9
+due_minute = 0
+due_section = "AM"
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#Given the current time and deadline time represented by the
+#variables above, determine if an assignment is still eligible
+#for submission. An assignment is eligible if the time
+#represented by current_hour, current_minute, and
+#current_section is before the time represented by due_hour,
+#due_minute, and due_section.
+
+
+#Add your code here!
